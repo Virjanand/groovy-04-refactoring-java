48c2a1af85bd3297ab706cafb2568fa3e4ff5303
Virjanand <virjanand@virjanand.com>
2020-08-07 05:35:33 +0200

    Starting point: plain Java for loop with printing to console.

---
+++ src/main/groovy/Greetings.groovy
@@ -0,0 +1,11 @@
+package main.groovy
+
+class Greetings {
+    static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("ho");
+        }
+
+        System.out.println("Merry Groovy!");
+    }
+}
