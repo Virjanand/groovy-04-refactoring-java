b052c45714450974faaa1bd2a1f2c53af30ee9f5
Virjanand <virjanand@virjanand.com>
2020-08-08 05:40:15 +0200

    remove semicolons

--- src/main/groovy/Greetings.groovy
+++ src/main/groovy/Greetings.groovy
@@ -3,9 +3,9 @@ package main.groovy
 class Greetings {
     static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
-            System.out.print("ho ");
+            System.out.print("ho ")
         }
 
-        System.out.println("Merry Groovy!");
+        System.out.println("Merry Groovy!")
     }
 }
