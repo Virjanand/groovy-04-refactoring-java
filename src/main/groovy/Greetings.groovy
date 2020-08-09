45a8460709741d84381d6916872599ede70c0014
Virjanand <virjanand@virjanand.com>
2020-08-09 12:30:54 +0200

    Remove class and method definitions

--- src/main/groovy/Greetings.groovy
+++ src/main/groovy/Greetings.groovy
@@ -1,11 +1,5 @@
-package main.groovy
-
-class Greetings {
-    static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            System.out.print("ho ")
-        }
-
-        System.out.println("Merry Groovy!")
-    }
+for (int i = 0; i < 3; i++) {
+    System.out.print("ho ")
 }
+
+System.out.println("Merry Groovy!")
