094701346152de0d34f2d9926fcc47f5710f2dc1
Virjanand <virjanand@virjanand.com>
2020-08-11 05:35:00 +0200

    Rewrite using Groovy GDK extensions

--- src/main/groovy/ExecuteProcess.groovy
+++ src/main/groovy/ExecuteProcess.groovy
@@ -1,17 +1,3 @@
 package main.groovy
 
-class ExecuteProcess {
-    static void main(String[] args) {
-        try {
-            Process process = Runtime.getRuntime().exec("git help");
-            BufferedReader result = new BufferedReader(
-                    new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = result.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-}
+println "git help".execute().text
