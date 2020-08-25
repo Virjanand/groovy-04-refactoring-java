15083d3949f158b917247cec0f52f81c338c3028
Virjanand <virjanand@virjanand.com>
2020-08-25 06:04:45 +0200

    Named parameters and automatic map conversion

---
+++ src/main/groovy/NamedParameters.groovy
@@ -0,0 +1,14 @@
+package main.groovy
+
+class Robot {
+    def type, height, width
+
+    def access(Map location, weight, fragile) {
+        println "Received fragile? $fragile, weight: $weight, $location"
+    }
+}
+
+robot = new Robot(type: 'arm', width: 10, height: 40)
+println "$robot.type, $robot.height, $robot.width"
+
+robot.access(x: 30, y: 20, z: 10, 50, true)
