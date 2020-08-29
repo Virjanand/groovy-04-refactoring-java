5338f088fa522a4bf332893f7614881bb51faa83
Virjanand <virjanand@virjanand.com>
2020-08-29 08:49:39 +0200

    Multiple return values/assignments

--- src/main/groovy/OptionalParameters.groovy
+++ src/main/groovy/OptionalParameters.groovy
@@ -7,3 +7,11 @@ def log(x, base = 10) {
 println log(1024)
 println log(1024, 10)
 println log(1024, 2)
+
+def task(name, String[] details) {
+    println "$name - $details"
+}
+
+task 'Call', '123-456-7890'
+task 'Call', '123-456-7890', '231-546-0987'
+task 'Check mail'
